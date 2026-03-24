3c512bb2ee2ab57fc47470acbe577f1d642d2414
Arham Shah <86466418+Arhamshah18@users.noreply.github.com>
2026-03-24 10:30:37 +0530

    Create shapes.py

---
+++ Unit 3/shapes.py
@@ -0,0 +1,9 @@
+#Main program , which is imported as a user-defined module.
+import math
+def circle_area(r):
+    return math.pi*r*r
+def rectangle_area(l, b):
+    return l*b
+def triangle_area(l, b):
+    return 0.5*l*b
+  
